cc52e83fc6db928a1f7ba71cd9e7c0478c6d2cc7
Micro <Micro@DESKTOP-QDKO2MA>
2023-09-30 09:48:36 -0300

    Adicionar arquivos de projeto.

---
+++ Aplicação/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola, qual o seu nome?");
+            int a = ReadLine();
+
+        }
+    }
+}
