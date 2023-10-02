ff254cc0deb12bfaf25b18b7db70107b196303c4
Guilherme Oliveira <guilhermzx@hotmail.com>
2023-10-02 15:14:03 -0300

    Atualização 1.0

--- Aplicação/Program.cs
+++ Aplicação/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,53 @@ namespace Aplicação
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ola, qual o seu nome?");
-            int a = ReadLine();
+            menu();
+        }
 
+
+
+
+        void menu()
+        {
+            Console.WriteLine("Olá, qual operação deseja fazer?");
+            Console.ReadLine();
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão\n");
+            Console.WriteLine("Por favor, digite o numero da opção que deseja:");
+
+            opcoes();
+            int num = int.Parse(Console.ReadLine());
+
+        }
+
+
+        void opcoes()
+        {
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Você Escolheu a adição!");
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    Console.WriteLine("Você Escolheu a Subtração");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Você Escolheu a Multiplicação");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Você Escolheu a divisão");
+                    break;
+
+                default:
+                    Console.WriteLine("Não foi possivel escolher a sua opção, desculpe.");
+                    break;
+            }
         }
     }
 }
